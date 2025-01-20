918ccd27881d1896853d71db5d924a3dbcf7a232
Arunponugotii <97236440+Arunponugotii@users.noreply.github.com>
2025-01-20 11:55:05 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,6 +15,8 @@ provider "google" {
 resource "google_container_cluster" "primary" {
   name     = var.cluster_name
   location = var.region
+  node_config {
+    service_account = "githubactions-sa@turnkey-guild-441104-f3.iam.gserviceaccount.com"
 
   initial_node_count = var.node_count
 
